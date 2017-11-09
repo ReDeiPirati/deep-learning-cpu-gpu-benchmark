c30672a0dac3da59a626e166328b8ab8744da159
ReDeiPirati <alessio.gozzoli@gmail.com>
2017-11-09 18:45:28 +0100

    Add notebook to plot result

--- floydhub_keras_cpu_gpu_benchmark.py
+++ floydhub_keras_cpu_gpu_benchmark.py
@@ -1,6 +1,8 @@
 """
 Launcher Script for benchmarking FloydHub Service(gpu and cpu) on:
 
+(Must be logged in on FH to run this)
+
 - MLP on MNIST
 - ConvNet on MNIST
 - ConvNet on CIFAR-10
@@ -16,7 +18,7 @@ PLATFORM_TYPE = ["cpu", "gpu"]
 
 # Test file script list
 test_files = [f for f in os.listdir("test_files") if f.endswith('.py')]
-test_files.remove('CustomCallback.py') # exclude the Logger Class
+test_files.remove('CustomCallback.py')  # exclude the Logger Class
 
 # Prepare Floyd commands
 floyd_cmd = "floyd run --env tensorflow-1.3"
@@ -28,12 +30,13 @@ for test_file in test_files:
 		# Help in Web dashboard visualization
 		message = "{}_{}".format(test_file[:-3], platform)
 		# Command Template
-		# floyd run --env .. --(gpu|cpu) --message testfile_plat "python test/script (cpu|gpu)"
-		command = floyd_cmd + " --{p} --message {m} ".format(p=platform, m=message) + \
+		# floyd run --env .. --(gpu|cpu) --message testfile_plat
+		# "python test/script (cpu|gpu)"
+		command = floyd_cmd + \
+			" --{p} --message {m} ".format(p=platform, m=message) + \
 			"python test_files/{} {}".format(test_file, platform)
 
-		if test_file.startswith("lstm"):
-			# Command to run
-			print(command)
-			subprocess.call(command.split(), shell=False)  # Safe
-			# TODO: auto csv-log retrieval after jobs ending.
+		# Command to run
+		print(command)
+		subprocess.call(command.split(), shell=False)  # Safe
+		# TODO: auto csv-log retrieval after jobs ending.
